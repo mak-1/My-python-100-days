86f71195e28fa199bdd5482510b88e2bc7a5be8e
Mak <mak-1@hotmail.it>
2022-12-02 15:01:15 +0000

    fixed bug on mappa

--- mappa.py
+++ mappa.py
@@ -2,7 +2,7 @@
 row1 = ["⬜️","️⬜️","️⬜️"]
 row2 = ["⬜️","⬜️","️⬜️"]
 row3 = ["⬜️️","⬜️️","⬜️️"]
-map = [row1, row2, row3]
+mappa = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
 position = input("Where do you want to put the treasure? ")
 # 🚨 Don't change the code above 👆
@@ -13,7 +13,7 @@ horizontal =int(position_str[0])
 vertical =int(position_str[1])
 
 
-selected_row=[vertical-1]
+selected_row=mappa[vertical-1]
 selected_row[horizontal-1] = 'X'
 
 #map[vertical-1][horizontal-1] = 'X'
